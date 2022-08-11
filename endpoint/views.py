bc2bbfc1305a10b1e814c643a9a5991bd596722e
bazhil <i-bazh@mail.ru>
2022-08-12 01:51:20 +0300

    Add some CRUD views. Works, but not stable. Need test and refactoring.

--- endpoint/views.py
+++ endpoint/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from .models import Venicle, Categories
 from .serializers import VenicleSerializer
 
+from django.http.response import JsonResponse
+from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
 class AutoAPIList(generics.ListCreateAPIView):
@@ -20,11 +25,65 @@ class AutoAPIUpdate(generics.ListCreateAPIView):
     # authentication_classes = (TokenAuthentication, )
 
 
-class AutoAPIDelete(generics.ListCreateAPIView):
-    queryset = Venicle.objects.all()
-    serializer_class = VenicleSerializer
-    permission_classes = (IsAdminUser, )
+# class AutoAPIDelete(generics.ListCreateAPIView):
+#     queryset = Venicle.objects.all()
+#     serializer_class = VenicleSerializer
+#     permission_classes = (IsAdminUser, )
 
-# TODO: Добавить view с возможностью искать по БД
 
 # TODO: Добавить view с возможностью загружать и выгружать в csv, xlsx
+
+@api_view(['GET'])
+def view_venicles(request):
+    """
+    TODO: не работает, исследовать и починить!
+    """
+
+    # checking for the parameters from the URL
+    if request.query_params:
+        items = Venicle.objects.filter(**request.query_param.dict())
+    else:
+        items = Venicle.objects.all()
+
+    # if there is something in items else raise error
+    if items:
+        data = VenicleSerializer(items)
+
+        return Response(data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['POST'])
+def add_venicle(request):
+    venicle = VenicleSerializer(data=request.data)
+
+    # validating for already existing data
+    if Venicle.objects.filter(**request.data).exists():
+        raise VenicleSerializer.ValidationError('This data already exists')
+
+    if venicle.is_valid():
+        venicle.save()
+        return Response(venicle.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['POST'])
+def update_venicle(request, pk):
+    item = Venicle.objects.get(pk=pk)
+    data = VenicleSerializer(instance=item, data=request.data)
+
+    if data.is_valid():
+        data.save()
+        return Response(data.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['DELETE'])
+def delete_venicle(request, pk):
+    item = get_object_or_404(Venicle, pk=pk)
+    item.delete()
+    return Response(status=status.HTTP_202_ACCEPTED)
+
--- search_auto/urls.py
+++ search_auto/urls.py
@@ -1,23 +1,9 @@
-"""search_auto URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# coding: utf-8
 from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
-from endpoint.views import AutoAPIList, AutoAPIUpdate, AutoAPIDelete
-
+from endpoint.views import AutoAPIList, AutoAPIUpdate
+from endpoint.views import add_venicle, update_venicle, delete_venicle
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,7 +11,13 @@ urlpatterns = [
     path('api/user-auth/', include('rest_framework.urls')),
     path('api/auto/list', AutoAPIList.as_view()),
     path('api/auto/<int:pk>', AutoAPIUpdate.as_view()),
-    path('api/auto/delete/<int:pk>', AutoAPIDelete.as_view()),
+    # path('api/auto/delete/<int:pk>', AutoAPIDelete.as_view()),
+
+    path('api/auto/create', add_venicle, name='add-venicle'),
+    # path('api/auto/list', view_venicles, name='view_venicles'),
+    path('api/auto/update/<int:pk>', update_venicle, name='update-venicle'),
+    path('api/auto/delete/<int:pk>', delete_venicle, name='delete-venicle'),
+
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
