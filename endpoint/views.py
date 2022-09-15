4175ad8eb117085b444244cfda11435822f673a9
bazhil <i-bazh@mail.ru>
2022-09-15 20:28:50 +0300

    Lazy finish

--- endpoint/views.py
+++ endpoint/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
-from .models import Venicle, Categories
+from .models import Venicle
 from .serializers import VenicleSerializer
 
 from django.http.response import FileResponse
@@ -36,33 +36,6 @@ class AutoAPIUpdate(generics.ListCreateAPIView):
     # authentication_classes = (TokenAuthentication, )
 
 
-# class AutoAPIDelete(generics.ListCreateAPIView):
-#     queryset = Venicle.objects.all()
-#     serializer_class = VenicleSerializer
-#     permission_classes = (IsAdminUser, )
-
-
-@api_view(['GET'])
-def view_venicles(request):
-    """
-    TODO: не работает, исследовать и починить!
-    """
-
-    # checking for the parameters from the URL
-    if request.query_params:
-        items = Venicle.objects.filter(**request.query_param.dict())
-    else:
-        items = Venicle.objects.all()
-
-    # if there is something in items else raise error
-    if items:
-        data = VenicleSerializer(items)
-
-        return Response(data)
-    else:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-
 @api_view(['POST'])
 def add_venicle(request):
     venicle = VenicleSerializer(data=request.data)
@@ -84,6 +57,16 @@ def update_venicle(request, pk):
 
     if data.is_valid():
         data.save()
+        return Response(data.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['GET'])
+def view_venicle(request, pk):
+    item = Venicle.objects.get(pk=pk)
+    data = VenicleSerializer(instance=item)
+    if data:
         return Response(data.data)
     else:
         return Response(status=status.HTTP_404_NOT_FOUND)
@@ -112,8 +95,6 @@ class ExportImportExcel(APIView):
         df = pd.read_excel(request.FILES['files'])
         for venicle in df.values:
             try:
-                # TODO: Загружается!!! Следует разобраться почему падает ошибка: При загрузке данных из excel-файла произошла ошибка:
-                #  Failed lookup for key [categories] in <Venicle: BMW X7 2016>
                 data = VenicleSerializer(data={
                     # 'id': venicle[0],
                     'mark': venicle[1],
@@ -149,8 +130,6 @@ class ExportImportCSV(APIView):
 
     def post(self, request):
         df = pd.read_csv(request.FILES['files'])
-        # TODO: Загружается!!! Следует разобраться почему падает ошибка: При загрузке данных из excel-файла произошла ошибка:
-        #  Failed lookup for key [categories] in <Venicle: BMW X7 2016>
         for venicle in df.values:
             try:
                 data = VenicleSerializer(data={
--- search_auto/urls.py
+++ search_auto/urls.py
@@ -3,7 +3,7 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
-from endpoint.views import AutoAPIList, AutoAPIUpdate, ExportImportExcel, ExportImportCSV
+from endpoint.views import AutoAPIList, ExportImportExcel, ExportImportCSV, view_venicle
 from endpoint.views import add_venicle, update_venicle, delete_venicle
 from search.views import PaginatedElasticSearchAPIView
 
@@ -11,15 +11,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('endpoint.urls')),
     path('api/user-auth/', include('rest_framework.urls')),
-    path('api/auto/list', AutoAPIList.as_view()),
-    path('api/auto/<int:pk>', AutoAPIUpdate.as_view()),
-    # path('api/auto/delete/<int:pk>', AutoAPIDelete.as_view()),
-
+    path('api/auto', AutoAPIList.as_view()),
+    path('api/auto/<int:pk>', view_venicle, name='view-venicle'),
     path('api/auto/create', add_venicle, name='add-venicle'),
-    # path('api/auto/list', view_venicles, name='view_venicles'),
     path('api/auto/update/<int:pk>', update_venicle, name='update-venicle'),
     path('api/auto/delete/<int:pk>', delete_venicle, name='delete-venicle'),
-
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
